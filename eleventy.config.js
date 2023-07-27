c1a3cfdc89bbca803db4d7bddd58bcbe5c32e328
Jason Garber <jason@sixtwothree.org>
2023-07-26 22:23:45 -0400

    Add and configure Sass plugin

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,7 +2,13 @@ module.exports = function(eleventyConfig) {
   // Passthrough File Copy
   eleventyConfig
     .addPassthroughCopy('./src/*.{ico,txt}')
-    .addPassthroughCopy('./src/assets');
+    .addPassthroughCopy('./src/assets/images')
+    .addPassthroughCopy('./src/assets/javascripts')
+    .addPassthroughCopy('./src/assets/octicons')
+    .addPassthroughCopy('./src/assets/videos');
+
+  // Plugins
+  eleventyConfig.addPlugin(require('./lib/plugins/sass.js'));
 
   return {
     dir: {
