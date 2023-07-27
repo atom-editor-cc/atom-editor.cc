c625439ccd43b582c4d15d1e1da4f1ebedfed90e
Jason Garber <jason@sixtwothree.org>
2023-07-26 22:18:39 -0400

    Remove unnecessary addressfield.min.json file

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,7 +2,6 @@ module.exports = function(eleventyConfig) {
   // Passthrough File Copy
   eleventyConfig
     .addPassthroughCopy('./src/*.{ico,txt}')
-    .addPassthroughCopy('./src/addressfield.min.json')
     .addPassthroughCopy('./src/assets');
 
   return {
