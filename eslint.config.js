5ba5beb1b74b42f7ead4732dd91b6c57d570bcdb
Jason Garber <jason@sixtwothree.org>
2023-07-28 16:29:31 -0400

    Add globals package, configure ESLint for browser globals

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,15 @@
 const config = require('@jgarber/eslint-config');
+const globals = require('globals');
 
 module.exports = [
   ...config,
   {
-    ignores: ['_site/*', 'src/assets/*']
+    ignores: ['_site/*']
+  },
+  {
+    files: ['src/assets/javascripts/**/*.js'],
+    languageOptions: {
+      globals: globals.browser
+    }
   }
 ];
