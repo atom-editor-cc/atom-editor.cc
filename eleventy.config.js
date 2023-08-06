3d402b2b5818f5add09cfc79e0f40215a99e7c98
Jason Garber <jason@sixtwothree.org>
2023-08-05 22:43:37 -0400

    Add and configure libraries, filters, and excerpt parsing

--- eleventy.config.js
+++ eleventy.config.js
@@ -9,6 +9,19 @@ module.exports = function(eleventyConfig) {
       './node_modules/octicons/octicons/octicons.{eot,svg,ttf,woff}': 'assets/fonts'
     });
 
+  // Data
+  eleventyConfig.setFrontMatterParsingOptions({
+    excerpt: true,
+    excerpt_separator: '<!--more-->'
+  });
+
+  // Libraries
+  eleventyConfig.setLibrary('md', require('./lib/libraries/markdown.js'));
+  eleventyConfig.setLiquidOptions(require('./lib/libraries/liquid.js'));
+
+  // Filters
+  eleventyConfig.addFilter('markdown', require('./lib/filters/markdown.js'));
+
   // Plugins
   eleventyConfig.addPlugin(require('./lib/plugins/sass.js'));
 
