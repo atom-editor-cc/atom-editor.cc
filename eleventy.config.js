01f1fde972bb9c43b5333a6a9a358c19810e4550
Jason Garber <jason@sixtwothree.org>
2024-01-17 23:44:34 -0500

    Migrate to @jgarber/eleventy-plugin-markdown

--- eleventy.config.js
+++ eleventy.config.js
@@ -34,13 +34,16 @@ module.exports = function(eleventyConfig) {
     });
 
   // Libraries
-  eleventyConfig.setLibrary('md', require('./lib/libraries/markdown.js'));
   eleventyConfig.setLiquidOptions(require('./lib/libraries/liquid.js'));
 
-  // Filters
-  eleventyConfig.addFilter('markdown', require('./lib/filters/markdown.js'));
-
   // Plugins
+  eleventyConfig.addPlugin(require('@jgarber/eleventy-plugin-markdown'), {
+    plugins: [
+      [require('markdown-it-anchor'), { tabIndex: false }],
+      require('markdown-it-attrs')
+    ]
+  });
+
   eleventyConfig.addPlugin(require('./lib/plugins/sass.js'));
 
   return {
